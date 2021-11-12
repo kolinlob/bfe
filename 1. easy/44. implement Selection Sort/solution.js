eff200f8c41ce45fa84934a2069fc4bfdd5bfb39
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-12 22:43:44 +0200

    44. implement Selection Sort

---
+++ 1. easy/44. implement Selection Sort/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ */
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++)
+        if (arr[min] > arr[j])
+            min = j;
+
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+}
