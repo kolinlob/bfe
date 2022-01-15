a4a6cfc90fd68012fc3d1e9ef2ed9efd1d4a8542
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-16 00:46:53 +0200

    60. create your own `new` operator

---
+++ 1. easy/60. create your own `new` operator/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Function} constructor
+ * @param {any[]} args - argument passed to the constructor
+ * `myNew(constructor, ...args)` should return the same as `new constructor(...args)`
+ */
+const myNew = (constructor, ...args) => {
+  let obj = Object.create(constructor.prototype);
+
+  return constructor.apply(obj, args) ?? obj;
+};
