4c98c148069acc4942a99b7f9cc90e6f18523b5b
Ihor Nikora <ihor.nikora@gmail.com>
2022-02-06 15:20:05 +0200

    70. implement Observable.from()

---
+++ 2. medium/70. implement Observable.from()/solution.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Array | ArrayLike | Promise | Iterable | Observable} input
+ * @return {Observable}
+ */
+function from(input) {
+  if (Array.isArray(input) || typeof input[Symbol.iterator] === "function")
+    return new Observable((s) => fromArray(input, s));
+
+  if ("length" in input)
+    return new Observable((s) => fromArray(Array.from(input), s));
+
+  if (input instanceof Promise)
+    return new Observable((s) => fromPromise(input, s));
+
+  if (input instanceof Observable)
+    return new Observable((s) => input.subscribe(s));
+
+  function fromPromise(promise, subscriber) {
+    promise
+      .then(
+        (value) => subscriber.next(value),
+        (reason) => subscriber.error(reason)
+      )
+      .catch((error) => subscriber.error(error))
+      .finally(() => subscriber.complete());
+  }
+
+  function fromArray(input, subscriber) {
+    try {
+      for (let item of input) subscriber.next(item);
+    } catch (error) {
+      subscriber.error(error);
+    } finally {
+      subscriber.complete();
+    }
+  }
+}
